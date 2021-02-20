45ac8a183e48dc2f3831908041c04540b559d22a
Saheed Ogunsolu <saheedogunsolu2016@gmail.com>
2021-02-20 15:31:21 +0100

    final

--- src/components/sample/Group.js
+++ src/components/sample/Group.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Sample from "./Sample";
 import image1 from "./Ellipse22.png";
+import image2 from "./image.png";
+import image3 from "./image1.png";
+import image4 from "./image3.png";
+import image5 from "./image4.png";
+import image6 from "./image5.png";
 import "./sample.scss";
 const Group = () => {
   return (
@@ -13,31 +18,31 @@ const Group = () => {
       />
       <Sample
         name={"Adetola Fashina"}
-        image={image1}
+        image={image4}
         town={"ibadan"}
         customer={"customer"}
       />
       <Sample
         name={"Emmanuel Fayemi"}
-        image={image1}
+        image={image3}
         town={"akoka"}
         customer={"customer"}
       />
       <Sample
         name={"Chisom Obilor"}
-        image={image1}
+        image={image6}
         town={"magodo"}
         vendor={"vendor"}
       />
       <Sample
         name={"Adunoluwa Adeyemi"}
-        image={image1}
+        image={image5}
         town={"iwo road"}
         vendor={"vendor"}
       />
       <Sample
         name={"Chidi Okeke"}
-        image={image1}
+        image={image2}
         town={"shomolu"}
         vendor={"vendor"}
       />
--- src/components/sample/Group2.js
+++ src/components/sample/Group2.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Sample from "./Sample";
-import image1 from "./Ellipse22.png";
+import image1 from "./image11.png";
+import image2 from "./image12.png";
+import image3 from "./image13.png";
+import image4 from "./image14.png";
+import image5 from "./image15.png";
+import image6 from "./image16.png";
 import "./sample.scss";
 const Group = () => {
   return (
     <div className="flex group">
-      <Sample name={"Joseph Ike"} image={image1} vendor={"vendor"} />
-      <Sample name={"Adetola Fashina"} image={image1} vendor={"vendor"} />
-      <Sample name={"Emmanuel Fayemi"} image={image1} vendor={"vendor"} />
-      <Sample name={"Chisom Obilor"} image={image1} vendor={"vendor"} />
-      <Sample name={"Adunoluwa Adeyemi"} image={image1} vendor={"vendor"} />
-      <Sample name={"Chidi Okeke"} image={image1} vendor={"vendor"} />
+      <Sample name={"Temi Obadofin"} image={image6} vendor={"vendor"} />
+      <Sample name={"Promise Ejiofor"} image={image4} vendor={"vendor"} />
+      <Sample name={"Feyisola Arinola"} image={image2} vendor={"vendor"} />
+      <Sample name={"Karen Ibeh"} image={image5} vendor={"vendor"} />
+      <Sample name={"Oluchi Uzo"} image={image3} vendor={"vendor"} />
+      <Sample name={"Amos Okafor"} image={image1} vendor={"vendor"} />
     </div>
   );
 };
